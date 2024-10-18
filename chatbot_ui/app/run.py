f7ab55677a4525886941e4067f458adecc7b0560
Juan Pereira <hperezrjb@gmail.com>
2024-10-18 13:25:39 -0700

    Pass tool call and app function response back in metadata (#193)

--- chatbot_ui/app/run.py
+++ chatbot_ui/app/run.py
@@ -2,14 +2,21 @@ import json
 import os
 from openai import OpenAI, DefaultHttpxClient
 import gradio as gr
-import logging as log
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+log = logging.getLogger(__name__)
+
 CHAT_COMPLETION_ENDPOINT = os.getenv("CHAT_COMPLETION_ENDPOINT")
 CURVE_STATE_HEADER = "x-curve -state"
-log.info("CHAT_COMPLETION_ENDPOINT: ", CHAT_COMPLETION_ENDPOINT)
+log.info(f"CHAT_COMPLETION_ENDPOINT: {CHAT_COMPLETION_ENDPOINT}")
 
 client = OpenAI(
     api_key="--",
@@ -23,23 +30,19 @@ def predict(message, state):
         state["history"] = []
     history = state.get("history")
     history.append({"role": "user", "content": message})
-    log.info("history: ", history)
+    log.info(f"history: {history}")
 
     # Custom headers
     custom_headers = {
         "x-curve -deterministic-provider": "openai",
     }
 
-    metadata = None
-    if "curve _state" in state:
-        metadata = {CURVE_STATE_HEADER: state["curve _state"]}
-
     try:
         raw_response = client.chat.completions.with_raw_response.create(
             model="--",
             messages=history,
             temperature=1.0,
-            metadata=metadata,
+            # metadata=metadata,
             extra_headers=custom_headers,
         )
     except Exception as e:
@@ -49,26 +52,35 @@ def predict(message, state):
         log.info("Error calling gateway API: {}".format(e.message))
         raise gr.Error("Error calling gateway API: {}".format(e.message))
 
-    log.info("raw_response: ", raw_response.text)
+    log.error(f"raw_response: {raw_response.text}")
     response = raw_response.parse()
 
     # extract curve _state from metadata and store it in gradio session state
     # this state must be passed back to the gateway in the next request
     response_json = json.loads(raw_response.text)
-    curve _state = None
     if response_json:
-        metadata = response_json.get("metadata", {})
-        if metadata:
-            curve _state = metadata.get(CURVE_STATE_HEADER, None)
-    if curve _state:
-        state["curve _state"] = curve _state
+        # load curve _state from metadata
+        curve _state_str = response_json.get("metadata", {}).get(CURVE_STATE_HEADER, "{}")
+        # parse curve _state into json object
+        curve _state = json.loads(curve _state_str)
+        # load messages from curve _state
+        curve _messages_str = curve _state.get("messages", "[]")
+        # parse messages into json object
+        curve _messages = json.loads(curve _messages_str)
+        # append messages from curve  gateway to history
+        for message in curve _messages:
+            history.append(message)
 
     content = response.choices[0].message.content
 
     history.append({"role": "assistant", "content": content, "model": response.model})
+
+    # for gradio UI we don't want to show raw tool calls and messages from developer application
+    # so we're filtering those out
+    history_view = [h for h in history if h["role"] != "tool" and "content" in h]
     messages = [
-        (history[i]["content"], history[i + 1]["content"])
-        for i in range(0, len(history) - 1, 2)
+        (history_view[i]["content"], history_view[i + 1]["content"])
+        for i in range(0, len(history_view) - 1, 2)
     ]
     return messages, state
 
